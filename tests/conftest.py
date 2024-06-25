9f3b7a29e0693f4c498b7fbb58da5b94abe89198
Naseem <test@gmail.com>
2024-06-25 06:25:17 +0500

    Indian timezone new changes

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,9 +9,10 @@ from webdriver_manager.chrome import ChromeDriverManager
 def setup(request):
     option = Options()
     option.add_experimental_option("detach", True)
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=option)
-    driver.maximize_window()
-    driver.implicitly_wait(10)
+    option.add_argument("start-maximized")
+    service_obj = Service("/opt/homebrew/bin/chromedriver")
+    driver = webdriver.Chrome(service=service_obj, options=option)
+    driver.implicitly_wait(15)
     driver.get("http://staging.shopping.beeyor.com/shop/")
     request.cls.driver = driver
     yield
