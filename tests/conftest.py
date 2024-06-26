40a438cc5e5a74dfc80ce2334a94555b41653037
Naseem <test@gmail.com>
2024-06-26 07:41:33 +0500

    browser change

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,18 +3,30 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+
+driver = None
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser-name", action="store", default="chrome")
 
 
 @pytest.fixture(scope="class")
 def setup(request):
+    global driver
     option = Options()
     option.add_experimental_option("detach", True)
-    option.add_argument("start-maximized")
-    # service_obj = Service("/opt/homebrew/bin/chromedriver")
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=option)
-    driver.implicitly_wait(15)
-    driver.get("http://staging.shopping.beeyor.com/shop/")
+    browser_name = request.config.getoption("--browser-name")
+    if browser_name == "chrome":
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=option)
+        driver.get("http://staging.shopping.beeyor.com/shop/")
+    elif browser_name == "firefox":
+        driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+        driver.get("http://staging.shopping.beeyor.com/shop/")
+    driver.maximize_window()
+    driver.implicitly_wait(10)
+
     request.cls.driver = driver
     yield
     driver.quit()
-
