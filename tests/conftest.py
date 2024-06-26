184f8833c584f67bb3c95f3246e01a3acf2429fd
Naseem <test@gmail.com>
2024-06-26 08:42:41 +0500

    new change to master

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,8 +10,7 @@ def setup(request):
     option = Options()
     option.add_experimental_option("detach", True)
     option.add_argument("start-maximized")
-    service_obj = Service("/opt/homebrew/bin/chromedriver")
-    driver = webdriver.Chrome(service=service_obj, options=option)
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=option)
     driver.implicitly_wait(15)
     driver.get("http://staging.shopping.beeyor.com/shop/")
     request.cls.driver = driver
